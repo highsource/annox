4f3d7b0c108d438bfd5cd306ca55c12a8b3965dc
highsource <aleksei.valikov@gmail.com>
2014-08-18 23:07:05 +0200

    =Version 1.0.0

--- release.bat
+++ release.bat
@@ -1,2 +1,19 @@
-call mvn release:prepare
-call mvn release:perform
+call mvn versions:set -DnewVersion=%1
+pause
+call mvn versions:commit
+pause
+call mvn clean install -DperformRelease
+pause
+rem  -Psamples -Ptests -Pdist
+git commit -a -m="Version %1"
+pause
+rem call mvn scm:tag -Dtag=%1
+git tag -a %1 -m 'Version %1'
+pause
+call mvn -DperformRelease -Psonatype-oss-release clean deploy
+pause
+call mvn versions:set -DnewVersion=%2
+call mvn versions:commit
+call mvn clean install -DperformRelease
+git commit -a -m="Version %2"
+pause
